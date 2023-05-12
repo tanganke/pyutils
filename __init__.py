19b132c42a6b92087f2f8266176172dd1f78a178
tanganke <tang.anke@foxmail.com>
2023-05-12 18:09:48 +0800

    format

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ misc usefull functions and classes.
 """
 from . import collections, logging, path
 from .args import *
-from .logging import titled_log, TitledLog
+from .logging import TitledLog, titled_log
 from .timeit import *
 
 
